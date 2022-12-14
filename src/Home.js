ef5da4462121d2dce73a1eee272a9fa3d8dfbe0a
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-12-14 17:00:14 -0600

    Added dumb text that I won't touch

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () =>{
     return (
         <div>
-            <h1>I am Home Page</h1>
+            <h1>This is the Home Page</h1>
+            <div>
+                <p>There's not really anything to look at here.</p>
+                <p>I could explain myself or shop...</p>
+                <p>Or you could just start clicking around.</p>
+                <p>Try "Shop" at the top and never click back here.</p>
+            </div>
         </div>
     )
 }
