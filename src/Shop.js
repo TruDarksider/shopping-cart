7ef1c39a60db3cf8f27c1bfef0ba74a63d005de7
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-12-15 16:47:00 -0600

    Refactored shop to use items.json

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,36 @@
+import items from './items.json'
+
 const Shop = () =>{
+    const quantity = 1;
     return (
         <div>
             <h1>I am Shop Page</h1>
+            <div className="cardContainer">
+            {items.map((item) => {
+                return (
+                    <div className="card">
+                        <div className="cardHeader">
+                            <h3 key={item.id}>{item.name}</h3>
+                            <span>${item.price}</span>
+                        </div>
+                        <p>{item.description}</p>
+                        <div className="addToCart">
+                        {quantity === 0 ? (
+                            <button>+ Add To Cart</button>
+                        ) : 
+                            (
+                                <div className='changeQuantity'>
+                                    <button>+</button>
+                                    <input type='number' value={quantity} />
+                                    <button>-</button>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+                );
+            })}
+            </div>
         </div>
     )
 }
