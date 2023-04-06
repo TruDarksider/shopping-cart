bea0a123b0c6b801a76d0ec7ac80ac1fbf62ac99
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-04-06 15:49:19 -0500

    Now displays price, subtotal, and total

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,24 +2,38 @@ import items from './items.json'
 
 const ShoppingCart = (props) =>{
     const { cartItems } = props;
+    let total = 0;
+    for (const item of cartItems) {
+        total += matchIdtoItem(item.id).price * item.quantity;
+    }
 
     function matchIdtoItem(idToMatch){
 
-        return cartItems.find(({id}) => id === idToMatch ) ? items[idToMatch-1].name : null;
+        return cartItems.find(({id}) => id === idToMatch ) ? items[idToMatch-1] : null;
     }
 
     return (
             <div className="shoppingCart">
-                <h3>I am cart</h3>
+                <h1>Your Cart:</h1>
                 <div className='cartDetails'>
                     {cartItems.map((cartItem) => {
                         return (
                         <div className='cartItemDetails'>
-                            <h2 key={cartItem.id}>{matchIdtoItem(cartItem.id)}</h2>
-                            <div>{'Qty: ' + cartItem.quantity}</div>
+                            <h2 key={cartItem.id}>{matchIdtoItem(cartItem.id).name}</h2>
+                                <div>{'Qty: ' + cartItem.quantity + ' at ' + matchIdtoItem(cartItem.id).price.toLocaleString('us-US', { style: 'currency', currency: 'USD' }) + '/ea'}
+                                    <div className='subtotal'>
+                                        <span>{(matchIdtoItem(cartItem.id).price * cartItem.quantity).toLocaleString('us-US', { style: 'currency', currency: 'USD' })}</span>
+                                    </div>
+                                </div>
                         </div>
                         );
                     })}
+                </div>
+                <div>
+                    -------------------------------------------------------------------
+                <h3>Total:
+                    <div className='total'>{total.toLocaleString('us-US', { style: 'currency', currency: 'USD' })}</div>
+                </h3>
                 </div>
             </div>
     );
